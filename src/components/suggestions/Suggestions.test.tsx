92333cf67f0144739a8b4657b86ef53980b61a89
elia9090 <elianarducciweb@gmail.com>
2021-01-26 03:25:01 +0100

    test suggestion init

--- src/components/suggestions/Suggestions.test.tsx
+++ src/components/suggestions/Suggestions.test.tsx
@@ -2,5 +2,6 @@ import { render, screen } from '@testing-library/react';
 import { Suggestions } from './Suggestions';
 
 test('render Suggestion', () => {
-
+  render(<Suggestions hidden={false} suggestions={[]} textInput={''}/>);
+  expect(screen.getByText('Search')).toBeInTheDocument();
 });
